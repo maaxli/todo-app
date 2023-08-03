58daab3ea42aac78c838d2194c14473f6d2bc5fd
Max L <122556386+maaxli@users.noreply.github.com>
2023-08-03 13:28:30 -0400

    Create layout and header ui

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,15 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
         ],
